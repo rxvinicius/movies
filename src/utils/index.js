b9fd0d9dfaa3cf8ca55c6bf6556dbc30feef77d6
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2023-11-04 15:19:27 -0300

    feat: getVoteAverage global function

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,19 +3,25 @@ const arraySize = (size, array) => {
   const arrayTemp = [];
 
   for (let i = 0; i < size; i++) {
-    if (array[i])
-      arrayTemp.push(array[i]);
+    if (array[i]) arrayTemp.push(array[i]);
   }
 
   return arrayTemp;
 };
 
 // Generates a random number based in array size
-const arrayRandomIndex = (array) => {
+const arrayRandomIndex = array => {
   return Math.floor(Math.random() * array.length);
+};
+
+const getVoteAverage = movie => {
+  const { vote_average } = movie;
+  const vote = vote_average || '0';
+  return `${parseInt(vote)}/10`;
 };
 
 module.exports = {
   arraySize,
   arrayRandomIndex,
+  getVoteAverage,
 };
