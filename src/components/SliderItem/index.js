367cabf208cfc4d148be2b5b90d657b2ef1f22c2
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2023-11-04 15:18:53 -0300

    feat: SliderItem propTypes

--- src/components/SliderItem/index.js
+++ src/components/SliderItem/index.js
@@ -1,10 +1,12 @@
 import { Container, BannerItem, Title, RateContainer, Rate } from './styles';
 import { MOVIE_POSTER_PATH_URL } from '../../shared/constants';
 import Star from '../Star';
+import { getVoteAverage } from '../../utils';
+import PropTypes from 'prop-types';
 
 export default function SliderItem(params) {
   const { data, navigatePage } = params;
-  const { original_title, poster_path, vote_average } = data;
+  const { original_title, poster_path } = data;
 
   return (
     <Container activeOpacity={0.7} onPress={navigatePage}>
@@ -13,8 +15,13 @@ export default function SliderItem(params) {
       <Title numberOfLines={1}>{original_title}</Title>
       <RateContainer>
         <Star />
-        <Rate>{vote_average}/10</Rate>
+        <Rate>{getVoteAverage(data)}</Rate>
       </RateContainer>
     </Container>
   );
 }
+
+SliderItem.propTypes = {
+  data: PropTypes.object.isRequired,
+  navigatePage: PropTypes.func.isRequired,
+};
