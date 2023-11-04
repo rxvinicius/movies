727e178a7e18dc37e96c6dc3d8d7ae96f4f14ff9
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2023-11-04 15:18:23 -0300

    feat: Header propTypes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,22 @@
-import { Container, MenuButton, Title } from './styles'
-import Feather from '@expo/vector-icons/Feather'
-import COLORS from '../../styles/colors'
-import { useNavigation } from '@react-navigation/native'
+import { Container, MenuButton, Title } from './styles';
+import Feather from '@expo/vector-icons/Feather';
+import COLORS from '../../styles/colors';
+import { useNavigation } from '@react-navigation/native';
+import PropTypes from 'prop-types';
 
 export default function Header({ title }) {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
     <Container>
       <MenuButton onPress={() => navigation.openDrawer()}>
-        <Feather name='menu' size={36} color={COLORS.WHITE} />
+        <Feather name="menu" size={36} color={COLORS.WHITE} />
       </MenuButton>
       <Title>{title}</Title>
     </Container>
-  )
+  );
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
