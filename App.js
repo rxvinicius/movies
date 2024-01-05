82154c6e200764deab690ceb05facd044ba7e7f2
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-01-05 20:47:01 -0300

    feat: changed app name / splash screen / app name

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import store from './src/redux/store';
 import Routes from './src/routes';
+import { setBackgroundColorAsync } from 'expo-system-ui';
+import COLORS from './src/styles/colors';
 
 export default function App() {
+  setBackgroundColorAsync(COLORS.SECONDARY);
   let persistor = persistStore(store);
 
   return (
